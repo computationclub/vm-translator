dd19759d8ba5307cdb2f8a73f2ce6299b13a9aa3
Leo Cassarani <leo.cassarani@me.com>
2015-03-24 19:58:09 +0000

    Implement a simple CodeWriter#write_arithmetic for adding numbers

--- lib/code_writer.rb
+++ lib/code_writer.rb
@@ -1,2 +1,28 @@
-module CodeWriter
+class CodeWriter
+  def initialize(output)
+    @output = output
+  end
+
+  def set_file_name(filename)
+  end
+
+  def write_arithmetic(command)
+    output.puts <<-EOF
+      @SP
+      // SP--
+      MD=M-1
+      // Load M[SP]
+      A=M
+      D=M
+      // Load M[SP-1]
+      A=A-1
+      // Add M[SP] to M[SP-1]
+      // Store results in M[SP-1]
+      M=D+M
+    EOF
+  end
+
+  private
+
+  attr_reader :output
 end
