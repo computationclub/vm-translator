ce4d055e1fcc36fd9d82b5adf942abc6370932c5
Leo Cassarani <leo.cassarani@me.com>
2015-03-24 20:17:07 +0000

    Implement CodeWriter#write_arithmetic for the eq command

--- lib/code_writer.rb
+++ lib/code_writer.rb
@@ -7,19 +7,53 @@ class CodeWriter
   end
 
   def write_arithmetic(command)
-    output.puts <<-EOF
-      @SP
-      // SP--
-      MD=M-1
-      // Load M[SP]
-      A=M
-      D=M
-      // Load M[SP-1]
-      A=A-1
-      // Add M[SP] to M[SP-1]
-      // Store results in M[SP-1]
-      M=D+M
-    EOF
+    case command
+    when 'add'
+      output.puts <<-EOF
+        @SP
+        // SP--
+        MD=M-1
+        // Load M[SP]
+        A=M
+        D=M
+        // Load M[SP-1]
+        A=A-1
+        // Add M[SP] to M[SP-1]
+        // Store results in M[SP-1]
+        M=D+M
+      EOF
+    when 'eq'
+      output.puts <<-EOF
+        @SP
+        // SP--
+        MD=M-1
+        // Load M[SP]
+        A=M
+        D=M
+        // Load M[SP-1]
+        A=A-1
+        // Subtract M[SP] from M[SP-1]
+        D=D-M
+        // If the result == 0 then jump to (EQUAL)
+        @EQUAL
+        D;JEQ
+        // Load M[SP]
+        @SP
+        A=M-1
+        // M[SP-1] = 0
+        M=0
+        // Jump to (END)
+        @END
+        0;JMP
+        (EQUAL)
+        // Load M[SP]
+        @SP
+        A=M-1
+        // M[SP-1] = -1
+        M=-1
+        (END)
+      EOF
+    end
   end
 
   def write_push_pop(command, segment, index)
