6c24141cd7f5eb582a3bb38333f3a8962dd5f4e9
Tom Stuart <tom@codon.com>
2015-03-28 20:56:20 +0000

    Extract filename manipulations into ExampleFromDirectory class

--- spec/acceptance/translator_spec.rb
+++ spec/acceptance/translator_spec.rb
@@ -1,7 +1,6 @@
-require 'fileutils'
 require 'open3'
-require 'pathname'
 require 'tmpdir'
+require 'support/example_from_directory'
 require 'support/helpers/emulation_helper'
 
 RSpec.describe 'the translator' do
@@ -10,42 +9,22 @@ RSpec.describe 'the translator' do
   TRANSLATOR_PATH = File.expand_path('../../../bin/translator', __FILE__)
   EXAMPLES_PATH = File.expand_path('../examples', __FILE__)
 
-  EXT = { input: '.vm', script: '.tst' }
-
-  Pathname.new(EXAMPLES_PATH).children.select(&:directory?).each do |directory_pathname|
-    base_filename = directory_pathname.basename
-    base_pathname = directory_pathname + base_filename
-    script_pathname = base_pathname.sub_ext(EXT[:script])
-    script_filename = script_pathname.basename
-
-    script = File.read(script_pathname)
-    output_filename = script.slice(/load (?<filename>[^,\s]+)/, :filename)
-    actual_filename = script.slice(/output-file (?<filename>[^,\s]+)/, :filename)
-    expected_filename = script.slice(/compare-to (?<filename>[^,\s]+)/, :filename)
-
-    it "generates a #{output_filename} file which satisfies #{script_filename}" do
-      input_pathname = base_pathname.sub_ext(EXT[:input])
-      output, error, status = Open3.capture3(TRANSLATOR_PATH, input_pathname.to_path)
+  ExampleFromDirectory.all_in(EXAMPLES_PATH).each do |example|
+    it "generates a #{example.output_pathname.basename} file which satisfies #{example.script_pathname.basename}" do
+      output, error, status = Open3.capture3(TRANSLATOR_PATH, example.input_pathname.to_path)
       expect(output).not_to be_empty
       expect(error).to be_empty
       expect(status).to be_success
 
       Dir.mktmpdir do |temporary_directory|
-        FileUtils.cp_r directory_pathname, temporary_directory
-        directory_pathname = Pathname.new(temporary_directory) + base_filename
+        example = example.copy_into(temporary_directory)
 
-        script_pathname = directory_pathname + script_filename
-        output_pathname = directory_pathname + output_filename
-
-        File.write(output_pathname, output)
-        error, status = emulator.run(script_pathname.to_path)
+        File.write(example.output_pathname, output)
+        error, status = emulator.run(example.script_pathname.to_path)
 
         unless status.success?
           STDERR.write error
-
-          expected_pathname = directory_pathname + expected_filename
-          actual_pathname = directory_pathname + actual_filename
-          expect(File.read(actual_pathname)).to eq File.read(expected_pathname)
+          expect(File.read(example.actual_pathname)).to eq File.read(example.expected_pathname)
         end
       end
     end
